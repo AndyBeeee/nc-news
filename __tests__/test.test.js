a6cd659b076eac388cd795b71fcf54f1c5b9b4e6
AndyBeee <bradshawandy@yahoo.co.uk>
2023-06-27 11:12:58 +0100

    update test.test.js file to include test for generic bad path error handler

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -14,6 +14,17 @@ afterAll(() => {
     return db.end()
 })
 
+describe('Invalid GET paths return a 404 error', () => {
+    test('Invalid paths return a 404 error', () => {
+    return request(app)
+    .get('/invalid/path')
+    .expect(404)
+    .then(({ body }) => {
+    expect(body).toEqual({ status: 404, msg: 'Not Found' })
+      })
+  })
+})
+
 describe('GET /api/topics', () => {
     test('Status code 200 returned when passed a get request', () => {
     return request(app)
@@ -70,32 +81,31 @@ describe('GET /api/articles/:article_id', () => {
         .expect(200)
     })
     test('Request responds with an article in the correct format', () => {
-        const articleFormat = {
-        author: expect.any(String),
-        title: expect.any(String),
-        article_id: expect.any(Number),
-        body: expect.any(String),
-        topic: expect.any(String),
-        created_at: expect.any(String),
-        votes: expect.any(Number),
-        article_img_url: expect.any(String)
-        }
-        const expected = expect.objectContaining(articleFormat)
+        const expectedArticle = {
+            author: "icellusedkars",
+            title: "Sony Vaio; or, The Laptop",
+            article_id: 2,
+            body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+            topic: "mitch",
+            created_at: "2020-10-16T05:03:00.000Z",
+            votes: 0,
+            article_img_url: "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+          }
         return request(app)
         .get('/api/articles/2')
         .then(({ body }) => {
-            expect(body.article).toEqual(expected)
+            expect(body.article).toEqual(expectedArticle)
         })
     })
     test('Request responds with a status code 404 when passed a non-existing article_id', () => {
         return request(app)
-            .get('/api/articles/999') 
-            .expect(404)
-            .then(({ body }) => {
-            expect(body.msg).toEqual('Not Found')
-            })
-          })
-    test('Request responds with a 400 status for an invalid article_id', () => {
+        .get('/api/articles/999') 
+        .expect(404)
+        .then(({ body }) => {
+        expect(body.msg).toEqual('Not Found')
+        })
+    })
+    test('Request responds with a status code 400 when passed an invalid article_id', () => {
     return request(app)
         .get('/api/articles/notAnId')
         .expect(400)
