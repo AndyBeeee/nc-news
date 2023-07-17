9ae152c328b26fb99d40994ee4ac5c6ff3a9954a
AndyBeee <bradshawandy@yahoo.co.uk>
2023-07-17 18:50:38 +0100

    add body to selectAllArticles

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -25,7 +25,7 @@ exports.selectAllComments = () => {
 
 exports.selectAllArticles = () => {
     return db.query (
-        `SELECT author, title, article_id, topic, created_at, votes, article_img_url 
+        `SELECT author, title, body, article_id, topic, created_at, votes, article_img_url 
         FROM articles
         ORDER BY created_at DESC;`)
         .then(({ rows }) => {
